529a404e147d74dca9cb2e5b9b15da6970984f6b
Thomas Clavier <tom@tcweb.org>
2020-04-18 17:35:58 +0200

    ignore idea files and listen on all interfaces

--- src/main/java/fr/ulille/iut/agile/Main.java
+++ src/main/java/fr/ulille/iut/agile/Main.java
@@ -18,7 +18,7 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/api/v1/";
+    public static final String BASE_URI = "http://0.0.0.0:8080/api/v1/";
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     
     /**

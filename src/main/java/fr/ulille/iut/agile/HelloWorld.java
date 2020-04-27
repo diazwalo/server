387a1399480280b92c4a6861b6aa85fcfc817aa8
Thibault Garcia <thibault.garcia.etu@univ-lille.fr>
2020-04-27 16:52:43 +0200

    correction d'un commentaire

--- src/main/java/fr/ulille/iut/agile/HelloWorld.java
+++ src/main/java/fr/ulille/iut/agile/HelloWorld.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
- * Root resource (exposed at "myresource" path)
+ * Root resource (exposed at "helloworld" path)
  */
 @Path("helloworld")
 public class HelloWorld {

df1e8e0b921e266d3b1cb4fc6bc65c94e1c27585
Paksat <delplace.nicolas@protonmail.com>
2020-04-28 16:38:20 +0200

    Ajout de la création de 'luUID directement dans la classe utilisateur

--- src/main/java/fr/ulille/iut/agile/beans/Utilisateur.java
+++ src/main/java/fr/ulille/iut/agile/beans/Utilisateur.java
@@ -1,15 +1,19 @@
 package fr.ulille.iut.agile.beans;
 
+import java.util.UUID;
+
 public class Utilisateur {
 	
 	private String name;
 	private String mdp;
 	private String email;
-
+	private UUID identifiant;
+	
 	public Utilisateur(String pName, String pMdp, String pEmail) {
 		name = pName;
 		mdp = pMdp;
 		email = pEmail;
+		identifiant = UUID.randomUUID();
 	}
 
 	public String getName() {
@@ -34,6 +38,10 @@ public class Utilisateur {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public UUID getIdentifiant() {
+		return identifiant;
 	}
 
 	

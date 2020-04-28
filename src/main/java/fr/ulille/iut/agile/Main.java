c1ff99acd210ff12ef68de2e0e4389061cff22f7
Thibault Garcia <thibault.garcia.etu@univ-lille.fr>
2020-04-28 14:16:44 +0200

    correction de code smell

--- src/main/java/fr/ulille/iut/agile/Main.java
+++ src/main/java/fr/ulille/iut/agile/Main.java
@@ -44,9 +44,7 @@ public class Main {
      */
     public static void main(String[] args) throws InterruptedException {
         final HttpServer server = startServer();
-        if(LOGGER.isInfoEnabled()) {
-            LOGGER.info(String.format("Jersey app started with WADL available at " + "%sapplication.wadl", BASE_URI));
-        }
+        System.out.println(String.format("Jersey app started with WADL available at " + "%sapplication.wadl", BASE_URI));
         Thread.currentThread().join();
         server.shutdownNow();
     }

bf96a609e72e0e52a9a0ed8e2f1fbac57dd13816
Thibault Garcia <thibault.garcia.etu@univ-lille.fr>
2020-04-30 11:45:19 +0200

    [code smell] remplacement d'un syso par un logger

--- src/main/java/fr/ulille/iut/agile/DaoPatternDemo.java
+++ src/main/java/fr/ulille/iut/agile/DaoPatternDemo.java
@@ -4,13 +4,18 @@ import fr.ulille.iut.agile.beans.Utilisateur;
 import fr.ulille.iut.agile.dao.UtilisateurDao;
 import fr.ulille.iut.agile.dto.UtilisateurDaoImpl;
 
+import java.util.logging.Logger;
+
 public class DaoPatternDemo {
+
+	private static final Logger LOGGER = Logger.getLogger(DaoPatternDemo.class.getName());
+	
 	public static void main(String[] args) {
 		UtilisateurDao studentDao = new UtilisateurDaoImpl();
 
 		//print all students
 		for (Utilisateur utili : studentDao.lister()) {
-			System.out.println("Utilisateur: [email : " + utili.getEmail() + ", et son mdp : " + utili.getMdp() + " ]");
+			LOGGER.info("Utilisateur: [email : " + utili.getEmail() + ", et son mdp : " + utili.getMdp() + " ]");
 		}
 
 	}

e3b12e306a342bf98626cbfc582256e468139908
Paksat <delplace.nicolas@protonmail.com>
2020-05-04 11:24:38 +0200

    Correction erreur de création de table

--- src/main/java/fr/ulille/iut/agile/dao/MurDao.java
+++ src/main/java/fr/ulille/iut/agile/dao/MurDao.java
@@ -9,13 +9,13 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import fr.ulille.iut.agile.beans.Mur;
 
 public interface MurDao {
-//CREATE TABLE `Utilisateur` ( `name` TEXT, `mdp` TEXT, `email` TEXT, `identifiant` TEXT, PRIMARY KEY(`identifiant`) );
+//CREATE TABLE Utilisateur ( name TEXT, mdp TEXT, email TEXT, identifiant TEXT, PRIMARY KEY(identifiant) );
 
 
-	@SqlUpdate("CREATE TABLE Utilisateur(name TEXT, mdp TEXT, email TEXT, identifiant TEXT, PRIMARY KEY(identifiant)")
+	@SqlUpdate("CREATE TABLE Mur ( mno TEXT, nom TEXT, geolocalisation TEXT, superficie INTEGER, identifianUtili TEXT, FOREIGN KEY(identifianUtili) REFERENCES Utilisateur(identifiant), PRIMARY KEY(mno)")
 	void creationTable();
 	
-	@SqlUpdate("INSERT INTO Utilisateur(name,mdp, email, identifiant) VALUES(:name, :mdp, :email, :identifiant)")
+	@SqlUpdate("INSERT INTO Utilisateur(mno, nom, geolocalisation,superficie,identifianUtil) VALUES(:mno, :nom, :geolocalisation,:superficie, :identifianUtil)")
 	void ajouter(Mur cuve);
 	
 	@SqlQuery("SELECT * FROM mur")
@@ -27,6 +27,6 @@ public interface MurDao {
 	@SqlQuery("DELETE FROM mur WHERE identifiant = :mno")
 	void deleteMur(@Bind("mno") Mur mno);
 	
-	@SqlQuery("UPDATE mur SET nom = :nom WHERE id = :mno.getNom()")
+	@SqlQuery("UPDATE mur SET nom = :nom WHERE id = :mno")
 	void modifierNom(@Bind("mno")Mur mno, @Bind("nom")String nom);
 }

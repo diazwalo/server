29b607561be466dbd7b798314bd76dbe9cc4acbd
Paksat <delplace.nicolas@protonmail.com>
2020-05-04 12:07:16 +0200

    Ajout du sql

--- src/main/java/fr/ulille/iut/agile/dao/CuveDao.java
+++ src/main/java/fr/ulille/iut/agile/dao/CuveDao.java
@@ -2,12 +2,30 @@ package fr.ulille.iut.agile.dao;
 
 import java.util.List;
 
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
 import fr.ulille.iut.agile.beans.Cuve;
 
+
 public interface CuveDao {
+	
+	@SqlUpdate("CREATE TABLE Cuve (cno TEXT, capacite INTEGER, quantitéActuel INTEGER, nom TEXT, mno INTEGER, FOREIGN KEY(mno) REFERENCES Mur(mno), PRIMARY KEY(cno) ")
+	void creationTable();
+	
+	@SqlUpdate("INSERT INTO Utilisateur(cno,capacite, quantitéActuel, nom, mno) VALUES(:cno,:capacite, :quantitéActuel, :nom, :mno)")
 	void ajouter(Cuve cuve);
+	
+	@SqlQuery("SELECT * FROM cuve")
 	List<Cuve>lister();
-	Cuve getCuve(String uno);
-	void deleteCuve(Cuve uno);
-	void modifierNom(Cuve cuve, String nom);
+	
+	@SqlQuery("SELECT * FROM utilisateur WHERE identifiant = :cno")
+	Cuve getCuve(@Bind("cno") String cno);
+	
+	@SqlQuery("DELETE FROM cuve WHERE identifiant = :cno")
+	void deleteCuve(@Bind("cno") Cuve uno);
+	
+	@SqlQuery("UPDATE cuve SET nom = :nom WHERE id = :cuve")
+	void modifierNom(@Bind("cuve")Cuve cuve, @Bind("nom")String nom);
 }
--- src/main/java/fr/ulille/iut/agile/dao/UtilisateurDao.java
+++ src/main/java/fr/ulille/iut/agile/dao/UtilisateurDao.java
@@ -2,6 +2,7 @@ package fr.ulille.iut.agile.dao;
 
 import java.util.List;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
@@ -10,10 +11,10 @@ import fr.ulille.iut.agile.beans.Utilisateur;
 
 public interface UtilisateurDao {
 
-	@SqlUpdate("CREATE TABLE IF NOT EXISTS utilisateur (id INTEGER PRIMARY KEY, name VARCHAR UNIQUE NOT NULL)")
+	@SqlUpdate("CREATE TABLE Utilisateur ( name TEXT, mdp TEXT, email TEXT, identifiant TEXT, PRIMARY KEY(identifiant)")
 	void creationTable();
 
-	@SqlUpdate("INSERT INTO utilisateur (util) VALUES (:util)")
+	@SqlUpdate("INSERT INTO Utilisateur(name,mdp, email, identifiant) VALUES(:name, :mdp, :email, :identifiant)")
 	void ajouter(Utilisateur util);
 
 	@SqlQuery("SELECT * FROM utilisateur")
@@ -22,9 +23,10 @@ public interface UtilisateurDao {
 
 	@SqlQuery("SELECT * FROM utilisateur WHERE identifiant = :uno")
 	@RegisterBeanMapper(Utilisateur.class)
-	Utilisateur getUtilisateur(String uno);
+	Utilisateur getUtilisateur(@Bind("uno") String uno);
 	
 	@SqlQuery("DELETE FROM utilisateur WHERE identifiant = :uno")
 	@RegisterBeanMapper(Utilisateur.class)
-	void deleteUtilisateur(String uno);
+	void deleteUtilisateur(@Bind("uno") String uno);
 }
+
